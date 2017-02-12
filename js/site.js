3be05daaaeb0043d61e0b0580c4b5ebed8ebfc2a
Caspar Dunant <spadxiii@gmail.com>
2017-02-12 20:23:44 +0100

    Toggle previews

--- js/site.js
+++ js/site.js
@@ -1,9 +1,21 @@
 (function($) {
   var $navBar = $('header.nav');
+
   function collapseNavbar() {
     $navBar.toggleClass('collapse', $navBar.offset().top > $navBar.height());
   }
 
   $(window).on('scroll', collapseNavbar);
   collapseNavbar();
+
+  $('.projects article').each(function(index, $item) {
+    var $full = $('.screenshot img', $item);
+    var $previews = $('.previews a', $item);
+    $previews.on('click', function(event) {
+      event.preventDefault();
+      $('img', $previews).removeClass('active');
+      $('img', this).addClass('active');
+      $full.attr('src', this.href);
+    });
+  });
 })(jQuery);
